11eb2c9b8e3f814f6b14e13b63e1b105f4fdb65c
marialelela <131173320+marialelela@users.noreply.github.com>
2023-05-17 08:50:50 -0300

    Add files via upload

---
+++ acoes.js
@@ -0,0 +1,17 @@
+let resposta = window.document.getElementById('saida')
+
+function acao1() {
+    resposta.innerHTML += '<p>Clicou no primeiro botão</p>'
+}
+
+function acao2() {
+    resposta.innerHTML += '<p>Clicou no segundo botão</p>'
+}
+
+function acao3() {
+    resposta.innerHTML += '<p>Clicou no terceiro botão</p>'
+}
+
+function acao4() {
+    resposta.innerHTML += '<p>Clicou no quarto botão</p>'
+}
